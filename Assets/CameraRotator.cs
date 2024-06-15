2cd1d9470616793156a6d149deb5678968dd4230
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-06-15 18:08:56 +0530

    Camera rotation around target

--- Assets/CameraRotator.cs
+++ Assets/CameraRotator.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CameraRotator : MonoBehaviour
 {
+    [SerializeField] private Transform parent;
+    [SerializeField] private Transform child;
+
+    private float x = 0;
+    private float y = 0;
+
+    private Vector3 posiStore = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        posiStore = (child.position - parent.position);
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {       
+        this.x += Input.GetAxis("Mouse X");
+        this.y += Input.GetAxis("Mouse Y");
+
+        Quaternion rot1 = Quaternion.Euler(this.y, this.x, 0);
+
+        this.transform.rotation = rot1;
+
+        Vector3 direction = Quaternion.Euler(this.y, this.x, 0) * posiStore;
+        this.transform.position = direction;
     }
 }
